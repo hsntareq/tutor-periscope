611e2c5f9059bc4b098cb6274030ce746f52b93d
shewa12 <shewa12kpi@gmail.com>
2022-01-14 00:23:41 +0600

    code-standard: coding spacing, line indentation added

--- inc/Evaluation/Student_Course_Evaluation.php
+++ inc/Evaluation/Student_Course_Evaluation.php
@@ -12,36 +12,39 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Student_Course_Evaluation {
 
-    /**
-     * Handle dependencies hooks
-     */
-    public function __construct() {
-        /**
-         * Do action hook added
-         * file path: tutor/templates/single/course/review-form.php:33
-         *
-         * Add custom fields
-         */
-        add_action( 'tutor_before_rating_textarea', array(
-            __CLASS__,
-            'tutor_before_rating_textarea'
-        ) );
-    }
+	/**
+	 * Handle dependencies hooks
+	 */
+	public function __construct() {
+		/**
+		 * Do action hook added
+		 * file path: tutor/templates/single/course/review-form.php:33
+		 *
+		 * Add custom fields
+		 */
+		add_action(
+			'tutor_before_rating_textarea',
+			array(
+				__CLASS__,
+				'tutor_before_rating_textarea',
+			)
+		);
+	}
 
-    /**
-     * Load student course evaluation template
-     *
-     * @return void
-     *
-     * @since v1.0.0
-     */
-    public static function tutor_before_rating_textarea() {
-        /**
-         * Load course evaluation template facilitating 
-         * Tutor function
-         */
-        tutor_load_template_from_custom_path(
-            TUTOR_PERISCOPE_DIR_PATH . 'templates/frontend/course-evaluation.php'
-        );
-    }
+	/**
+	 * Load student course evaluation template
+	 *
+	 * @return void
+	 *
+	 * @since v1.0.0
+	 */
+	public static function tutor_before_rating_textarea() {
+		/**
+		 * Load course evaluation template facilitating
+		 * Tutor function
+		 */
+		tutor_load_template_from_custom_path(
+			TUTOR_PERISCOPE_DIR_PATH . 'templates/frontend/course-evaluation.php'
+		);
+	}
 }
