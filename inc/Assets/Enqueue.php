2f549a8b1a8ab864491426a488f09da77179f6de
shewa12 <shewa12kpi@gmail.com>
2022-01-15 20:21:42 +0600

    dependency: wp-i18n dependency added for js files

--- inc/Assets/Enqueue.php
+++ inc/Assets/Enqueue.php
@@ -27,7 +27,7 @@ class Enqueue {
      */
     public function enqueue_frontend_assets() {
         wp_enqueue_style( 'tutor-periscope-frontend', TUTOR_PERISCOPE_DIR_URL . '/assets/css/frontend.min.css', null, TUTOR_PERISCOPE_VERSION, 'all' );
-        wp_enqueue_script( 'tutor-periscope-frontend', TUTOR_PERISCOPE_DIR_URL . '/assets/js/frontend.js', array(), TUTOR_PERISCOPE_VERSION, true );
+        wp_enqueue_script( 'tutor-periscope-frontend', TUTOR_PERISCOPE_DIR_URL . '/assets/js/frontend.js', array( 'wp-i18n' ), TUTOR_PERISCOPE_VERSION, true );
     }
 
     /**
@@ -35,6 +35,6 @@ class Enqueue {
      */
     public function enqueue_backend_assets() {
         wp_enqueue_style( 'tutor-periscope-backend', TUTOR_PERISCOPE_DIR_URL . '/assets/css/backend.min.css', null, TUTOR_PERISCOPE_VERSION, 'all' );
-        wp_enqueue_script( 'tutor-periscope-backend', TUTOR_PERISCOPE_DIR_URL . '/assets/js/backend.js', array( 'jquery' ), TUTOR_PERISCOPE_VERSION, true );
+        wp_enqueue_script( 'tutor-periscope-backend', TUTOR_PERISCOPE_DIR_URL . '/assets/js/backend.js', array( 'jquery', 'wp-i18n' ), TUTOR_PERISCOPE_VERSION, true );
     }
 }
