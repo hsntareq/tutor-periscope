6d669c2c6cc077cb655bb988e3cc2427dc734c0e
shewa12 <shewa12kpi@gmail.com>
2022-01-16 21:09:10 +0600

    fix: coding errors

--- inc/Query/DB_Query.php
+++ inc/Query/DB_Query.php
@@ -11,60 +11,69 @@ namespace Tutor_Periscope\Query;
 /**
  * This class is for using from derived class.
  * Derived class must have $table_name property.
- * 
+ *
  * Extend this class and use basic DB query.
  */
 abstract class DB_Query {
 
-    /**
-     * Insert data in the instance table
-     *
-     * @param array $data, data to insert in the table
-     *
-     * @return bool, true on success false on failure
-     *
-     * @since v1.0.0
-     */
-    protected function insert( array $data ): bool {
-        global $wpdb;
-        //Sanitize text field.
-        $data = array_map( function( $value ) {
-            return sanitize_text_field( $value );
-        }, $data );
+	/**
+	 * Insert data in the instance table
+	 *
+	 * @param array $data, data to insert in the table
+	 *
+	 * @return bool, true on success false on failure
+	 *
+	 * @since v1.0.0
+	 */
+	protected function insert( array $data ): bool {
+		global $wpdb;
+		// Sanitize text field.
+		$data = array_map(
+			function( $value ) {
+				return sanitize_text_field( $value );
+			},
+			$data
+		);
 
-        $insert = $wpdb->insert(
-            $this->get_table(),
-            $data
-        );
-        return $insert ? true : false;
-    }
+		$insert = $wpdb->insert(
+			$this->get_table(),
+			$data
+		);
+		return $insert ? true : false;
+	}
 
-    /**
-     * Update data in the instance table
-     *
-     * @param array $data, data to update in the table
-     * @param array $where, condition array
-     *
-     * @return bool, true on success false on failure
-     *
-     * @since v1.0.0
-     */    
-    protected function update( array $data, array $where ): bool {
-        global $wpdb;
-        //Sanitize text field.
-        $data = array_map( function( $value ) {
-            return sanitize_text_field( $value );
-        }, $data );
+	/**
+	 * Update data in the instance table
+	 *
+	 * @param array $data, data to update in the table
+	 * @param array $where, condition array
+	 *
+	 * @return bool, true on success false on failure
+	 *
+	 * @since v1.0.0
+	 */
+	protected function update( array $data, array $where ): bool {
+		global $wpdb;
+		// Sanitize text field.
+		$data = array_map(
+			function( $value ) {
+				return sanitize_text_field( $value );
+			},
+			$data
+		);
 
-        $where = array_map( function( $value ) {
-            return sanitize_text_field( $value );
-        }, $where );
+		$where = array_map(
+			function( $value ) {
+				return sanitize_text_field( $value );
+			},
+			$where
+		);
 
-        $update = $wpdb->update(
-            $this->get_table(),
-            $data,
-            $where
-        );
-        return $update ? true : false;
-    }
+		$update = $wpdb->update(
+			$this->get_table(),
+			$data,
+			$where
+		);
+		return $update ? true : false;
+	}
 }
