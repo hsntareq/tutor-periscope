322b68658ef97ff9e65b8f3b8d7c59aedad30cd2
shewa12 <shewa12kpi@gmail.com>
2022-02-02 00:49:16 +0600

    new: course linear path enforcement added

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -77,6 +77,93 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
+/***/ }),
+
+/***/ "./assets/src/frontend/linear.js":
+/*!***************************************!*\
+  !*** ./assets/src/frontend/linear.js ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _ajax__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./ajax */ "./assets/src/frontend/ajax.js");
+
+const {
+  __
+} = wp.i18n;
+/**
+ * Check previous lesson complete status and not completed previous lesson
+ * then restrict user to start next lesson or quiz
+ *
+ * @since v1.0.0
+ */
+
+document.addEventListener('DOMContentLoaded', async function () {
+  const lessonSidebar = document.getElementById('tutor-lesson-sidebar-tab-content');
+
+  if (lessonSidebar) {
+    const hasActiveContent = document.querySelector('.tutor-single-lesson-items.active'); //check if user in on any course material like lesson, quiz etc
+
+    if (hasActiveContent) {
+      const currentContent = hasActiveContent.querySelector('a');
+      let contentId = 0;
+
+      if (currentContent.hasAttribute('data-lesson-id')) {
+        contentId = Number(currentContent.getAttribute('data-lesson-id'));
+      }
+
+      if (currentContent.hasAttribute('data-quiz-id')) {
+        contentId = Number(currentContent.getAttribute('data-quiz-id'));
+      }
+
+      checkPreviousContentStatus(contentId);
+    }
+  }
+  /**
+   * On click course content check if user completed previous content
+   */
+
+
+  if (lessonSidebar) {
+    lessonSidebar.onclick = e => {
+      const target = e.target;
+      let clickedTag = target;
+
+      if (clickedTag.tagName !== 'A') {
+        clickedTag = target.closest('a');
+      }
+
+      if (clickedTag.hasAttribute('data-lesson-id')) {
+        checkPreviousContentStatus(Number(clickedTag.getAttribute('data-lesson-id')));
+      }
+
+      if (clickedTag.hasAttribute('data-quiz-id')) {
+        checkPreviousContentStatus(Number(clickedTag.getAttribute('data-quiz-id')));
+      }
+    };
+  }
+});
+
+async function checkPreviousContentStatus(contentId) {
+  if (contentId > 0) {
+    //check if user complete previous course content
+    const formData = new FormData();
+    formData.set('content-id', contentId);
+    formData.set('action', 'tutor_periscope_previous_content_status');
+    formData.set('nonce', tp_data.nonce);
+    const response = await (0,_ajax__WEBPACK_IMPORTED_MODULE_0__["default"])(formData);
+
+    if (response.success) {} else {
+      if (response.data.url) {
+        tutor_toast(__('Fail', 'tutor-periscope'), __('Please complete previous lesson first!', 'tutor-periscope'), 'error');
+        window.location.href = response.data.url;
+      } else {
+        tutor_toast(__('Fail', 'tutor-periscope'), __('Previous content status check failed!', 'tutor-periscope'), 'error');
+      }
+    }
+  }
+}
+
 /***/ })
 
 /******/ 	});
@@ -143,6 +230,8 @@ var __webpack_exports__ = {};
   \*****************************************/
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _course_evaluation__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./course-evaluation */ "./assets/src/frontend/course-evaluation.js");
+/* harmony import */ var _linear__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./linear */ "./assets/src/frontend/linear.js");
+
 
 })();
 
--- assets/src/frontend/frontend.js
+++ assets/src/frontend/frontend.js
@@ -1,1 +1,2 @@
 import "./course-evaluation";
+import "./linear";
