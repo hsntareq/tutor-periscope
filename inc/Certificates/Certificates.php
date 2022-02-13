93e34fe74af3b3911e9d20ca9a96de1f3fb152cb
shewa12 <shewa12kpi@gmail.com>
2022-02-14 00:45:08 +0600

    new: save instructor info meta box data

--- inc/Certificates/Certificates.php
+++ inc/Certificates/Certificates.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Handles registering custom certificate templates
- * 
+ *
  * @package Tutor Periscope
  */
 
@@ -14,41 +14,41 @@ defined( 'ABSPATH' ) || exit;
  */
 class Certificates {
 
-    /**
-     * Register
-     */
-    public function register() {
-        add_action( 'save_post_courses', array( $this, 'save_periscope_course_meta' ), 10, 2 );
-        add_action( 'add_meta_boxes', array( $this, 'add_tutor_periscope_additional_metabox' ) );
-        add_filter( 'tutor_certificate_templates', array( $this, 'register_tutor_periscope_certificate_templates' ) );
-        add_action( 'tutor/dashboard_course_builder_form_field_after', array( $this, 'register_periscope_metabox_in_frontend' ) );
-        add_action( 'tutor_profile_edit_before_social_media', array( $this, 'insert_tutor_periscope_license_input' ) );
-        add_action( 'tutor_profile_update_before', array( $this, 'update_license_info' ) );
-    }
+	/**
+	 * Register
+	 */
+	public function register() {
+		add_action( 'save_post_courses', array( $this, 'save_periscope_course_meta' ), 10, 2 );
+		add_action( 'add_meta_boxes', array( $this, 'add_tutor_periscope_additional_metabox' ) );
+		add_filter( 'tutor_certificate_templates', array( $this, 'register_tutor_periscope_certificate_templates' ) );
+		add_action( 'tutor/dashboard_course_builder_form_field_after', array( $this, 'register_periscope_metabox_in_frontend' ) );
+		add_action( 'tutor_profile_edit_before_social_media', array( $this, 'insert_tutor_periscope_license_input' ) );
+		add_action( 'tutor_profile_update_before', array( $this, 'update_license_info' ) );
+	}
 
-    /**
-     * Certificate templates loader
-     * 
-     * @param array $templates
-     * 
-     * @return array $templates
-     */
-    public function register_tutor_periscope_certificate_templates( $templates ) {
-        $templates['periscope'] = array(
+	/**
+	 * Certificate templates loader
+	 *
+	 * @param array $templates
+	 *
+	 * @return array $templates
+	 */
+	public function register_tutor_periscope_certificate_templates( $templates ) {
+		$templates['periscope'] = array(
 			'name'        => __( 'Periscope', 'tutor-periscope' ),
-			'orientation' => 'portrait', //landscape, portrait
+			'orientation' => 'portrait', // landscape, portrait
 			'path'        => trailingslashit( TUTOR_PERISCOPE_DIR_PATH . 'templates/certificates/periscope' ),
 			'url'         => trailingslashit( TUTOR_PERISCOPE_DIR_URL . 'templates/certificates/periscope' ),
 		);
 
 		return $templates;
-    }
+	}
 
-    /**
-     * Tutor Periscope additional metabox callback
-     */
-    public function add_tutor_periscope_additional_metabox() {
-		
+	/**
+	 * Tutor Periscope additional metabox callback
+	 */
+	public function add_tutor_periscope_additional_metabox() {
+
 		add_meta_box(
 			'tutor-periscope-additional-data',
 			__( 'Tutor Periscope Additional Fields', 'tutor-periscope' ),
@@ -57,12 +57,12 @@ class Certificates {
 		);
 	}
 
-    /**
-     * Generate periscope additional fields
-     */
-    public function generate_tutor_periscope_additional_fields( $echo = true ) {
-        ob_start();
-		include  TUTOR_PERISCOPE_DIR_PATH . 'views/metabox/tutor-persicope-additional-data.php';
+	/**
+	 * Generate periscope additional fields
+	 */
+	public function generate_tutor_periscope_additional_fields( $echo = true ) {
+		ob_start();
+		include TUTOR_PERISCOPE_DIR_PATH . 'views/metabox/tutor-persicope-additional-data.php';
 		$content = ob_get_clean();
 
 		if ( $echo ) {
@@ -70,23 +70,22 @@ class Certificates {
 		} else {
 			return $content;
 		}
-    }
+	}
 
-    /**
-     * Register persicope metabox in the frontend builder
-     */
-    public function register_periscope_metabox_in_frontend() {
-        course_builder_section_wrap( $this->generate_tutor_periscope_additional_fields( $echo = false ), __( 'Tutor Periscope Additional Fields', 'tutor-periscope' ) );
-    }
+	/**
+	 * Register persicope metabox in the frontend builder
+	 */
+	public function register_periscope_metabox_in_frontend() {
+		course_builder_section_wrap( $this->generate_tutor_periscope_additional_fields( $echo = false ), __( 'Tutor Periscope Additional Fields', 'tutor-periscope' ) );
+	}
 
-    /**
-     * Save periscope course meta
-     */
-    public function save_periscope_course_meta( $post_ID, $post ) {
-
+	/**
+	 * Save periscope course meta
+	 */
+	public function save_periscope_course_meta( $post_ID, $post ) {
 		$periscope_additional_data_edit = tutils()->avalue_dot( '_tutor_periscope_course_additional_data_edit', $_POST );
 		if ( $periscope_additional_data_edit ) {
-            // Set learning objectives meta.
+			// Set learning objectives meta.
 			if ( ! empty( $_POST['learning_objectives'] ) ) {
 				$learning_objectives = wp_kses_post( $_POST['learning_objectives'] );
 				update_post_meta( $post_ID, '_tp_learning_objectives', $learning_objectives );
@@ -94,42 +93,74 @@ class Certificates {
 				delete_post_meta( $post_ID, '_tp_learning_objectives' );
 			}
 
-            // Set endorsements meta.
-            if ( ! empty( $_POST['course_endorsements'] ) ) {
+			// Set endorsements meta.
+			if ( ! empty( $_POST['course_endorsements'] ) ) {
 				$endorsements = wp_kses_post( $_POST['course_endorsements'] );
 				update_post_meta( $post_ID, '_tp_endorsements', $endorsements );
 			} else {
 				delete_post_meta( $post_ID, '_tp_endorsements' );
 			}
+			/**
+			 * Set profession & instructors info
+			 *
+			 * @since v1.0.0
+			 */
+			$student_profession = sanitize_text_field( $_POST['_tp_student_profession'] );
+			update_post_meta(
+				$post_ID,
+				'_tp_student_profession',
+				$student_profession
+			);
+			$instructors_info  = array();
+			$instructor_names  = $_POST['_tp_instructor_name'];
+			$instructor_titles = $_POST['_tp_instructor_title'];
+			$instructor_bio    = $_POST['_tp_instructor_bio'];
+
+			foreach ( $instructor_names as $k => $name ) {
+				array_push(
+					$instructors_info,
+					array(
+						'name'  => sanitize_text_field( $name ),
+						'title' => sanitize_text_field( $instructor_titles[ $k ] ),
+						'bio'   => sanitize_textarea_field( $instructor_bio[ $k ] ),
+					)
+				);
+			}
+			update_post_meta(
+				$post_ID,
+				'_tp_instructors_info',
+				serialize( $instructors_info )
+			);
 		}
 	}
 
-    /**
-     * Add license input
-     */
-    public function insert_tutor_periscope_license_input( $user ) { 
-        $license_number = get_user_meta( $user->ID, '_tutor_periscope_license_number', true );
-        ?>
-        <div class="tutor-form-row">
-            <div class="tutor-form-col-12">
-                <div class="tutor-form-group">
-                    <label>
-                        <?php esc_html_e( 'License Number', 'tutor-periscope' ); ?>
-                    </label>
-                    <input type="text" name="license_number" value="<?php echo esc_attr( $license_number ); ?>" placeholder="<?php esc_attr_e( 'License Number', 'tutor-periscope' ); ?>">
-                </div>
-            </div>
-        </div>
-        
-   <?php }
+	/**
+	 * Add license input
+	 */
+	public function insert_tutor_periscope_license_input( $user ) {
+		$license_number = get_user_meta( $user->ID, '_tutor_periscope_license_number', true );
+		?>
+		<div class="tutor-form-row">
+			<div class="tutor-form-col-12">
+				<div class="tutor-form-group">
+					<label>
+						<?php esc_html_e( 'License Number', 'tutor-periscope' ); ?>
+					</label>
+					<input type="text" name="license_number" value="<?php echo esc_attr( $license_number ); ?>" placeholder="<?php esc_attr_e( 'License Number', 'tutor-periscope' ); ?>">
+				</div>
+			</div>
+		</div>
+		
+		<?php
+	}
 
-   /**
-    * Update License info
-    */
-    public function update_license_info( $user_id ) {
-        $license_number = sanitize_text_field( tutor_utils()->input_old( 'license_number') );
-        if ( ! is_wp_error( $user_id ) ) {
+	/**
+	 * Update License info
+	 */
+	public function update_license_info( $user_id ) {
+		$license_number = sanitize_text_field( tutor_utils()->input_old( 'license_number' ) );
+		if ( ! is_wp_error( $user_id ) ) {
 			update_user_meta( $user_id, '_tutor_periscope_license_number', $license_number );
-        }
-    }
+		}
+	}
 }
--- views/metabox/tutor-persicope-additional-data.php
+++ views/metabox/tutor-persicope-additional-data.php
@@ -10,7 +10,6 @@ $endorsements        = get_post_meta( $course_id, '_tp_endorsements', true );
 $learning_objectives = get_post_meta( $course_id, '_tp_learning_objectives', true );
 $student_profession  = get_post_meta( $course_id, '_tp_student_profession', true );
 $instructors_info    = unserialize( get_post_meta( $course_id, '_tp_instructors_info', true ) );
-
 do_action( 'tutor_periscope_course_metabox_before_additional_data' ); ?>
 
 <div class="tutor-option-field-row">
@@ -61,12 +60,35 @@ do_action( 'tutor_periscope_course_metabox_before_additional_data' ); ?>
 	</div>
 	<div class="tutor-option-field" id="tutor-periscope-instructors-metabox">
 		<div style="display: flex; flex-direction: column; row-gap: 15px;">
-			<input type="text" name="_tp_instructor_name[]" class="tutor-form-control" placeholder="<?php esc_attr_e( 'Instructor name', 'tutor-periscope' ); ?>" value="" />
-			
-			<input type="text" name="_tp_instructor_title[]" placeholder="<?php esc_attr_e( 'Instructor title', 'tutor-periscope' ); ?>"/>
+			<input type="text" name="_tp_instructor_name[]" class="tutor-form-control" placeholder="<?php esc_attr_e( 'Instructor name', 'tutor-periscope' ); ?>" value="<?php echo isset( $instructors_info[0]['name'] ) ? esc_html( $instructors_info[0]['name'] ) : ''; ?>" />
+	
+			<input type="text" name="_tp_instructor_title[]" placeholder="<?php esc_attr_e( 'Instructor title', 'tutor-periscope' ); ?>" value="<?php echo isset( $instructors_info[0]['title'] ) ? esc_html( $instructors_info[0]['title'] ) : ''; ?>"/>
 	   
-			<textarea name="_tp_instructor_bio[]" rows="2" placeholder="<?php esc_attr_e( 'Instructor bio', 'tutor-periscope' ); ?>"></textarea>
+			<textarea name="_tp_instructor_bio[]" rows="2" placeholder="<?php esc_attr_e( 'Instructor bio', 'tutor-periscope' ); ?>"><?php echo isset( $instructors_info[0]['bio'] ) ? esc_textarea( $instructors_info[0]['bio'] ) : ''; ?></textarea>
 		</div>
+		<?php if ( is_array( $instructors_info ) && count( $instructors_info ) ) : ?>
+			<?php
+			foreach ( $instructors_info as $k => $instructor ) :
+				if ( 0 === $k ) {
+					continue;
+				}
+				?>
+				<div class="tutor-periscope-each-instructor-box" style="display: flex; flex-direction: column; row-gap: 15px; margin-top: 15px">
+	
+					<div style="display: flex; column-gap: 10px;">
+						<input type="text" name="_tp_instructor_name[]" class="tutor-form-control" placeholder="<?php esc_html_e( 'Instructor name', 'tutor-periscope' ); ?>" value="<?php echo esc_html( $instructor['name'] ); ?>" />
+						<span class="tutor-periscope-remove-instructor" style="color:tomato; cursor: pointer;"><?php esc_html_e( 'Remove', 'tutor-periscope' ); ?></span>
+					</div>
+
+					<input type="text" name="_tp_instructor_title[]" placeholder="
+					<?php
+					esc_html_e( 'Instructor title', 'tutor-periscope' );
+					?>" value="<?php echo esc_html( $instructor['title']); ?>"/>
+			   
+					<textarea name="_tp_instructor_bio[]" rows='2' placeholder="<?php esc_html_e( 'Instructor bio', 'tutor-periscope' ); ?>"><?php echo esc_textarea( $instructor['bio'] ); ?></textarea>
+				</div>
+			<?php endforeach; ?>
+		<?php endif; ?>
 	</div>
 </div>
 
