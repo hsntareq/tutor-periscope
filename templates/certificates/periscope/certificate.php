119304f8350a865b8e74a9560347e56eb1c24f2b
Hasan Tareq <hsntareq@gmail.com>
2022-02-22 00:30:24 +0600

    Evaluation form updated, Certificate designed

--- templates/certificates/periscope/certificate.php
+++ templates/certificates/periscope/certificate.php
@@ -4,6 +4,9 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		<title>Periscope Certificate Title</title>
 		<style type="text/css"><?php $this->pdf_style(); ?></style>
+		<style>
+			.certificate-content p{margin-bottom: 0;}
+		</style>
 	</head>
 	<body>
 		<div class="certificate-wrap">
@@ -15,7 +18,6 @@
 					$learning_objectives = get_post_meta( $course->ID, '_tp_learning_objectives', true );
 					$license_number      = get_user_meta( $user->ID, '_tutor_periscope_license_number', true );
 
-
 				if ( $durationHours ) {
 					$hour_text  = $durationHours . ' ';
 					$hour_text .= ( $durationHours > 1 ) ? __( 'hours', 'tutor-periscope' ) : __( 'hour', 'tutor-periscope' );
@@ -26,44 +28,65 @@
 					$min_text .= ( $durationMinutes > 1 ) ? __( 'minutes', 'tutor-periscope' ) : __( 'minute', 'tutor-periscope' );
 				}
 					$duration_text = $hour_text . ' ' . $min_text;
+					$default_sinature_id = tutor_utils()->get_option('tutor_cert_signature_image_id');
+					$signature_image_url = isset($default_sinature_id) ? wp_get_attachment_url($default_sinature_id) : $signature_image_url;
 				?>
-			   <section class="certificate-header">
+				<section class="certificate-header">
 					<header class="certificate-logo">
-						<img src="<?php echo esc_url( TUTOR_PERISCOPE_DIR_URL . 'assets/images/periscope-logo.png' ); ?>" alt="Periscope Logo" />
+						<img src="<?php echo esc_url( TUTOR_PERISCOPE_DIR_URL . 'assets/images/periscope-logo-text.png' ); ?>" alt="Periscope Logo" />
 					</header>
-			   </section>
-			   <section class="certificate-details">
+			   	</section>
+			   	<section class="certificate-details">
 				   <h3 align="center" class="certificate-title">
 					   <b>Certificate of Course Completion</b>
 				   </h3>
 				   <p><strong>Course:</strong> <span class="course-info"><?php esc_html_e( $course->post_title ); ?></span></p>
-				   <p><strong>Student:</strong> <span class="course-info"><?php esc_html_e( $user->display_name ); ?></span></p>
+				   <p><span>Student:</span> <span class="course-info"><?php esc_html_e( $user->display_name ); ?></span></p>
 				   <?php
+						$student_state = get_post_meta( $course->ID, '_tp_student_state', true );
 						$student_profession = get_post_meta( $course->ID, '_tp_student_profession', true );
 					?>
 				   <p>
-					   <strong>
+					   <span>
 						   Profession:
 						   <span class="course-info">
 								<?php echo esc_html( $student_profession ? $student_profession : '' ); ?>
 						   </span>
-					   </strong>
+					   </span>
 				   </p>
-				   <p><strong>Start Date:</strong> <span class="course-info">05/23/20</span></p>
-				   <p><strong>Completion Date:</strong> <span class="course-info"><?php esc_html_e( $completed_date ); ?></span></p>
+				   <p>
+					   <span>
+						   State:
+						   <span class="course-info">
+								<?php echo esc_html( $student_state ? $student_state : '' ); ?>
+						   </span>
+					   </span>
+				   </p>
+				   <p><span>Date Completed: </span> <span class="course-info"><?php esc_html_e( $completed_date ); ?></span></p>
+				   <p>
+					   <span>
+						   Contact Hours:
+						   <span class="course-info">
+								<?php echo esc_html( $student_state ? $student_state : '' ); ?>
+						   </span>
+					   </span>
+				   </p>
 			   </section>
 			   <?php
 				if ( ! empty( $learning_objectives ) ) :
 					$learning_objectives = explode( "\n", $learning_objectives );
 					?>
 			   <section class="learning-objectives">
-				   <h3>Learning Objectives and Goals:</h3>
+				   <h4>Learning Objectives:</h4>
 					<?php if ( is_array( $learning_objectives ) && count( $learning_objectives ) ) : ?>
-						<?php foreach ( $learning_objectives as $learning_objective ) : ?>
-						<ol>
-							<?php echo esc_html( $learning_objective ); ?>
-						</ol>
+						<table class="custom-ol">
+						<?php foreach ( $learning_objectives as $key => $learning_objective ) : ?>
+							<tr>
+								<td width="20"><?php echo esc_html( $key+1 ); ?>. </td>
+								<td><?php echo esc_html( $learning_objective ); ?></td>
+							</tr>
 						<?php endforeach; ?>
+						</table>
 					<?php endif; ?>
 			   </section>
 			   <?php endif; ?>
@@ -78,28 +101,26 @@
 					if ( is_array( $instructors ) && count( $instructors ) ) :
 						?>
 					<section>
-						<h3>Instructors</h3>
+						<h4>Instructors</h4>
 						<?php foreach ( $instructors as $instructor ) : ?>
 							<div style="margin-bottom: 10px;">
-								<div>
-									<span>
-										<?php echo esc_html( $instructor['name'] ); ?>,
-										<?php echo esc_html( $instructor['title'] ); ?>
-									</span>
-								</div>
-								<div style="margin-left: 40px">
+								<p>
+									<?php echo esc_html( $instructor['name'] ); ?>,
+									<?php echo esc_html( $instructor['title'] ); ?>
+								</p>
+								<p style="margin-left: 40px">
 									<?php echo esc_textarea( $instructor['bio'] ); ?>
-								</div>
+								</p>
 							</div>
 						<?php endforeach; ?>
 					</section>
 					<?php endif; ?>
-			   <?php
+			   	<?php
 				if ( ! empty( $endorsements ) ) :
 					$endorsements = explode( "\n", $endorsements );
 					?>
-			   <section class="instructors endorsements">
-				   <h3>Endorsements:</h3>
+			   	<section class="instructors endorsements">
+				   	<h4>Endorsements:</h4>
 					<?php if ( is_array( $endorsements ) && count( $endorsements ) ) : ?>
 						<?php foreach ( $endorsements as $endorsement ) : ?>
 						<ol>
@@ -110,29 +131,26 @@
 			   </section>
 			   <?php endif; ?>
 			   <section class="instructors">
-				   <h3>Signature</h3>
-				   <p><?php echo esc_html( $instructor_name ); ?></p>
+				   <h4>Signature</h4>
 			   </section>
 			   <section class="medbridge">
-					<p class="medbridge-info">Andrew Mickus, <em>Director of Course Development</em></p>
+					<img src="<?php echo esc_url( $signature_image_url ); ?>" height="50" alt="signature" />
+					<p class="medbridge-info"><?php echo esc_html( $instructor_name ); ?></p>
+					<p class="medbridge-info">Periscope Founder, CEO</p>
 					<p class="medbridge-info">1633 Westlake Avenue North, Suite 200, Seattle, WA 98109</p>
-					<p class="medbridge-info contact"><span class="phone"><img class="phone-icon" src="<?php echo esc_url( TUTOR_PERISCOPE_DIR_URL . 'assets/images/phone.png' ); ?>" /> (206) 216-5003 </span> <span class="email"> <img class="email-icon" src="<?php echo esc_url( TUTOR_PERISCOPE_DIR_URL . 'assets/images/email.png' ); ?>" /> <a href="mailto:support@medbridgeed.com">support@medbridgeed.com</a></span></p>
+					<p class="medbridge-info"><a href="mailto:admin@periscope365.com">admin@periscope365.com</a></p>
 			   </section>
+
 			   <section class="licensor-info">
-				   <img src="<?php echo esc_url( $signature_image_url ); ?>" height=30 alt="signature" />
-				   <h3>Physical Therapist Licensed In Virginia</h3>
-					<p><strong>Contact Hours:</strong> <span class="therapist-info"><?php esc_html_e( $duration_text ); ?></span></p>
-					<p><strong>License:</strong> <span class="therapist-info"><?php esc_html_e( $license_number ); ?></span></p>
-					<p><strong>State:</strong> <span class="therapist-info">Virginia</span></p>
+				   <h4>Continuing Education Approval</h4>
+				   <p>Approved by <u><?php echo $user->display_name; ?></u> Approval number: ______</p>
+			   </section>
+			   <section class="certificate-before-footer">
 					<p>Some state licensing boards do not require course pre-approval. Participant is responsible for understanding their requirements.</p>
 			   </section>
 			   <section class="certificate-footer">
-				   <div class="leftside-content">
-						<p>MedBridge Certificate of Completion </p>
-				   </div>
-				   <div class="rightside-content">
- 
-				   </div>
+				   <p class="full-address">Office 855.955.7365 | 548 Market Street #75842, San Francisco, CA 94104 | periscope365.com</p>
+				   <img src="<?php echo TUTOR_PERISCOPE_DIR_URL . 'assets/images/certificate-logo.png'; ?>" height="66" alt="signature" />
 			   </section>
 			</div>
 		</div>
