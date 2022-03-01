85ccede033d1c96dae93b9332938c8480a34fa03
shewa12 <shewa12kpi@gmail.com>
2022-03-02 00:44:25 +0600

    new: quiz template added

--- inc/UserRole/FilterDashboardMenu.php
+++ inc/UserRole/FilterDashboardMenu.php
@@ -10,7 +10,7 @@
 namespace Tutor_Periscope\UserRole;
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
 /**
@@ -18,33 +18,65 @@ if ( ! defined( 'ABSPATH' ) ) {
  * for add menu.
  */
 class FilterDashboardMenu {
-    
-    /**
-     * Register hooks
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        add_filter( 'tutor_dashboard/nav_items', array( $this, 'filter_menu' ), 10, 1 );   
-    }
-    
-    /**
-     * Filter menu
-     *
-     * @since v1.0.0
-     *
-     * @param  mixed $menu value to filter
-     *
-     * @return array containing key value pair of menu items
-     */
-    public function filter_menu( array $menu ): array {
-        $reviewer = new Reviewer;
-        if ( wc_current_user_has_role( $reviewer->role ) ) {
-            $menu = array(
-                'review-attempts'   => __( 'Review Attempts', 'tutor-periscope' )
-            );
-        }
-        return $menu;
-    }
+
+	protected $user_role;
+
+	/**
+	 * Register hooks
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		$reviewer        = new Reviewer();
+		$this->user_role = $reviewer->role;
+
+		add_filter( 'tutor_dashboard/nav_items', array( $this, 'filter_menu' ), 100, 1 );
+		add_filter( 'load_dashboard_template_part_from_other_location', array( $this, 'load_template' ), 100, 1 );
+	}
+
+	/**
+	 * Filter menu
+	 *
+	 * @since v1.0.0
+	 *
+	 * @param array $menu value to filter.
+	 *
+	 * @return array containing key value pair of menu items
+	 */
+	public function filter_menu( array $menu ): array {
+		if ( wc_current_user_has_role( $this->user_role ) ) {
+			$new_menu = apply_filters(
+				'tutor_periscope_frontend_dashboard_menu',
+				array(
+					'review-attempts' => __( 'Review Attempts', 'tutor-periscope' ),
+				)
+			);
+            $menu = $new_menu;
+		}
+		return $menu;
+	}
+
+	/**
+	 * Include quiz attempt template
+	 *
+	 * @since v1.0.0
+	 *
+	 * @param  string $template  template file path to alter.
+	 *
+	 * @return string
+	 */
+	public function load_template( string $template ): string {
+		global $wp_query;
+		$query_vars = $wp_query->query_vars;
+		if ( wc_current_user_has_role( $this->user_role ) ) {
+			if ( 'review-attempts' === $query_vars['attachment'] ) {
+				$quiz_attempt_template = TUTOR_PERISCOPE_DIR_PATH . 'templates/frontend/quiz-attempts.php';
+				if ( file_exists( $quiz_attempt_template ) ) {
+					$template = apply_filters( 'tutor_periscope_quiz_attempt_template', $quiz_attempt_template );
+				}
+			}
+		}
+		return $template;
+	}
+
 }
