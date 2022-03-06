42abbee0772907fd4704a7e8b27db5590a7019d0
shewa12 <shewa12kpi@gmail.com>
2022-03-06 18:16:52 +0600

    new: filter download button content if user not allowed to download

--- inc/Certificates/DownloadApproval.php
+++ inc/Certificates/DownloadApproval.php
@@ -28,6 +28,14 @@ if ( ! class_exists( 'DownloadApproval' ) ) {
 		use \Tutor_Periscope\Traits\NonceVerify;
 
 		/**
+		 * Meta key, course id need to be concat at the end
+		 * ex: '_tp_allow_user_to_download_certificate' . $course_id
+		 *
+		 * @var string
+		 */
+		const META_KEY = '_tp_allow_user_to_download_certificate';
+
+		/**
 		 * Register hooks
 		 *
 		 * @return void
@@ -38,6 +46,18 @@ if ( ! class_exists( 'DownloadApproval' ) ) {
 				array(
 					__CLASS__,
 					'download_approval',
+				)
+			);
+
+			/**
+			 * Custom filter hook added.
+			 * Hook path: tutor-pro/addons/tutor-certificate/classes/Certificate.php
+			 */
+			add_filter(
+				'tutor_certificate_button',
+				array(
+					__CLASS__,
+					'filter_download_button',
 				)
 			);
 		}
@@ -53,11 +73,38 @@ if ( ! class_exists( 'DownloadApproval' ) ) {
 			if ( self::verify_nonce( $_POST['nonce'] ) ) {
 				$update = update_user_meta(
 					sanitize_text_field( $_POST['user_id'] ),
-					'_tp_allow_user_to_download_certificate' . $_POST['course_id'],
+					self::META_KEY . $_POST['course_id'],
 					sanitize_text_field( $_POST['course_id'] )
 				);
 				$update ? wp_send_json_success() : wp_send_json_error();
 			}
 		}
+
+		/**
+		 * Filter download button
+		 *
+		 * @param string $content  content for filtering.
+		 *
+		 * @return string   filter content
+		 */
+		public static function filter_download_button( $content ) {
+			$user_id   = get_current_user_id();
+			$course_id = get_the_ID();
+			$post      = get_post( $course );
+			if ( $post && 'courses' === $post->post_type ) {
+				$is_allowed_download = get_user_meta( $user_id, self::META_KEY . $course_id );
+				if ( ! $is_allowed_download ) {
+					ob_start();
+					?>
+					<p>
+						<?php esc_html_e( 'You are not allowed to download certificate yet.', 'tutor-periscope' ); ?>
+					</p>
+					<?php
+					$content = ob_get_clean();
+				}
+			}
+			return $content;
+		}
 	}
+
 }
