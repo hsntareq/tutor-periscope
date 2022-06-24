4e4afc6551045268bf7ee3da43b340ab71735d58
Hasan Tareq <hsntareq@gmail.com>
2022-06-25 00:43:08 +0600

    evaluation report layout designed

--- views/admin/evaluation-statistics-report.php
+++ views/admin/evaluation-statistics-report.php
@@ -1,5 +1,93 @@
-<div>
-    <h2>
+<style>
+.evaluation_report * {
+    line-height: 2em;
+}
+.evaluation_report{
+    padding: 10px 50px;
+}
+.evaluation_report .pdf_title{
+    text-align:center;
+}
+.evaluation_report table{
+    width:100%;
+    margin-bottom: 30px;
+}
+.evaluation_report table tr td:first-child{
+    width:50%;
+}
+
+</style>
+
+<div class="report_template evaluation_report">
+    <h2 class="pdf_title">
         Periscope Evaluation Statistics Report
     </h2>
+    <p><strong>Title: </strong> We are eager to hear your opinion . Please use the scale below to grade the following areas.</p>
+    <p><strong>Presenter: </strong> Mike, Skyler, Steve</p>
+
+    <table>
+        <tr>
+            <td></td>
+            <td align="center">Lowest <br> 1</td>
+            <td align="center">2</td>
+            <td align="center">3</td>
+            <td align="center">4</td>
+            <td align="center">Highest <br> 5</td>
+            <td align="center">Not Apply</td>
+        </tr>
+        <tr>
+            <td>lorem one two three</td>
+            <td align="center">1</td>
+            <td align="center">2</td>
+            <td align="center">3</td>
+            <td align="center">4</td>
+            <td align="center">5</td>
+            <td align="center">23</td>
+        </tr>
+        <tr>
+            <td>lorem one two three</td>
+            <td align="center">1</td>
+            <td align="center">2</td>
+            <td align="center">3</td>
+            <td align="center">4</td>
+            <td align="center">5</td>
+            <td align="center">23</td>
+        </tr>
+    </table>
+
+    <table>
+        <tr>
+            <td></td>
+            <td align="center">Yes</td>
+            <td align="center">No</td>
+            <td align="center">N/A</td>
+        </tr>
+        <tr>
+            <td>lorem one two three</td>
+            <td align="center">1</td>
+            <td align="center">2</td>
+            <td align="center">3</td>
+        </tr>
+        <tr>
+            <td>lorem one two three</td>
+            <td align="center">1</td>
+            <td align="center">2</td>
+            <td align="center">3</td>
+        </tr>
+        <tr>
+            <td>lorem one two three</td>
+            <td align="center">1</td>
+            <td align="center">2</td>
+            <td align="center">3</td>
+        </tr>
+    </table>
+
+    <p><strong>Comments:</strong></p>
+    <ol>
+        <li>Be visible the whole time( unable to see speaker 1st half )</li>
+        <li>Be visible the whole time( unable to see speaker 1st half )</li>
+        <li>Be visible the whole time( unable to see speaker 1st half )</li>
+        <li>Be visible the whole time( unable to see speaker 1st half )</li>
+        <li>Be visible the whole time( unable to see speaker 1st half )</li>
+    </ol>
 </div>
