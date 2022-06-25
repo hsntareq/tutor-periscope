233949fc60e14df07a0b75c4ec4e21f7ce743081
shewa12 <shewa12kpi@gmail.com>
2022-06-25 21:27:19 +0600

    fix: template include issue fixed

--- inc/EvaluationReport/Report.php
+++ inc/EvaluationReport/Report.php
@@ -17,54 +17,55 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class Report {
 
-    /**
-     * Register Hooks
-     *
-     * @since v2.0.0
-     * 
-     * @return void
-     */
+	/**
+	 * Register Hooks
+	 *
+	 * @since v2.0.0
+	 *
+	 * @return void
+	 */
 	public function __construct() {
 		add_action( 'template_redirect', __CLASS__ . '::render_report' );
 	}
 
-    /**
-     * Render report in PDF
-     *
-     * @since v2.0.0
-     * 
-     * @return void
-     */
+	/**
+	 * Render report in PDF
+	 *
+	 * @since v2.0.0
+	 *
+	 * @return void
+	 */
 	public static function render_report(): void {
 		$action = $_GET['action'] ?? '';
 
-        $should_download = false;
-        if ( 'tp-evaluation-report-download' === $action ) {
-            $should_download = true;
-        }
+		$should_download = false;
+		if ( 'tp-evaluation-report-download' === $action ) {
+			$should_download = true;
+		}
 
-        // Prepare view file.
-        $view = '';
-        if ( 'tp-evaluation-report-download' === $action || 'tp-evaluation-report-view' === $action ) {
-            $view = trailingslashit( TUTOR_PERISCOPE_VIEWS . 'admin' ) . 'evaluation-statistics-report.php';
-        } else {
-            $view = trailingslashit( TUTOR_PERISCOPE_VIEWS . 'admin' ) . 'evaluation-report-summary.php';
-        }
+		// Prepare view file.
+		$view = '';
+		if ( 'tp-evaluation-report-download' === $action || 'tp-evaluation-report-view' === $action ) {
+			$view = trailingslashit( TUTOR_PERISCOPE_VIEWS . 'admin' ) . 'evaluation-statistics-report.php';
+		}
+		if ( 'tp-evaluation-report-summary' === $action ) {
+			$view = trailingslashit( TUTOR_PERISCOPE_VIEWS . 'admin' ) . 'evaluation-report-summary.php';
+		}
 
-        /**
-         * If user trigger report action then view file
-         * will be available then do as per action.
-         * 
-         * Otherwise do nothing. 
-         */
-        if ( file_exists( $view ) ) {
-            tutor_load_template_from_custom_path(
-                $view
-            );
-            $content = apply_filters( 'tutor_periscope_evaluation_statistics', ob_get_clean() );
-            $pdf_file_name = 'periscope-evaluation-report.pdf';
-            PDFManager::render( $content, $pdf_file_name, $should_download );
-            exit;
-        }
+		/**
+		 * If user trigger report action then view file
+		 * will be available then do as per action.
+		 *
+		 * Otherwise do nothing.
+		 */
+		if ( '' !== $view && file_exists( $view ) ) {
+			tutor_load_template_from_custom_path(
+				$view
+			);
+			$content       = apply_filters( 'tutor_periscope_evaluation_statistics', ob_get_clean() );
+			$pdf_file_name = 'periscope-evaluation-report.pdf';
+			PDFManager::render( $content, $pdf_file_name, $should_download );
+			exit;
+		}
 	}
 }
