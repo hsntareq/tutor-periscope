8fd27cd8a4227be831491dd218b2c7daafd60973
shewa12 <shewa12kpi@gmail.com>
2022-06-26 16:23:44 +0600

    update: field options filter hook provider

--- inc/FormBuilder/FieldOptions.php
+++ inc/FormBuilder/FieldOptions.php
@@ -25,43 +25,46 @@ class FieldOptions {
 	 * @return array options.
 	 */
 	public static function compare_field_options(): array {
-		return array(
+		return apply_filters(
+			'tutor_periscope_compare_field_options',
 			array(
-				'label'     => 1,
-				'help_text' => __( 'Lowest', 'tutor-periscope' ),
-				'value'     => 1,
-				'selected'  => false,
-			),
-			array(
-				'label'     => 2,
-				'help_text' => '',
-				'value'     => 2,
-				'selected'  => false,
-			),
-			array(
-				'label'     => 3,
-				'help_text' => '',
-				'value'     => 3,
-				'selected'  => false,
-			),
-			array(
-				'label'     => 4,
-				'help_text' => '',
-				'value'     => 4,
-				'selected'  => false,
-			),
-			array(
-				'label'     => 5,
-				'help_text' => __( 'Highest', 'tutor-periscope' ),
-				'value'     => 5,
-				'selected'  => true,
-			),
-			array(
-				'label'     => __( 'Not Apply', 'tutor-periscope' ),
-				'help_text' => '',
-				'value'     => 'Not Apply',
-				'selected'  => false,
-			),
+				array(
+					'label'     => 1,
+					'help_text' => __( 'Lowest', 'tutor-periscope' ),
+					'value'     => 1,
+					'selected'  => false,
+				),
+				array(
+					'label'     => 2,
+					'help_text' => '',
+					'value'     => 2,
+					'selected'  => false,
+				),
+				array(
+					'label'     => 3,
+					'help_text' => '',
+					'value'     => 3,
+					'selected'  => false,
+				),
+				array(
+					'label'     => 4,
+					'help_text' => '',
+					'value'     => 4,
+					'selected'  => false,
+				),
+				array(
+					'label'     => 5,
+					'help_text' => __( 'Highest', 'tutor-periscope' ),
+					'value'     => 5,
+					'selected'  => true,
+				),
+				array(
+					'label'     => __( 'Not Apply', 'tutor-periscope' ),
+					'help_text' => '',
+					'value'     => 'Not Apply',
+					'selected'  => false,
+				),
+			)
 		);
 	}
 
@@ -73,25 +76,28 @@ class FieldOptions {
 	 * @return array options.
 	 */
 	public static function vote_field_options(): array {
-		return array(
+		return apply_filters(
+			'tutor_periscope_vote_field_options',
 			array(
-				'label'     => __( 'Yes', 'tutor-periscope' ),
-				'help_text' => '',
-				'value'     => 'Yes',
-				'selected'  => false,
-			),
-			array(
-				'label'     => __( 'No', 'tutor-periscope' ),
-				'help_text' => '',
-				'value'     => 'No',
-				'selected'  => false,
-			),
-			array(
-				'label'     => __( 'N/A', 'tutor-periscope' ),
-				'help_text' => '',
-				'value'     => 'N/A',
-				'selected'  => false,
-			),
+				array(
+					'label'     => __( 'Yes', 'tutor-periscope' ),
+					'help_text' => '',
+					'value'     => 'Yes',
+					'selected'  => true,
+				),
+				array(
+					'label'     => __( 'No', 'tutor-periscope' ),
+					'help_text' => '',
+					'value'     => 'No',
+					'selected'  => false,
+				),
+				array(
+					'label'     => __( 'N/A', 'tutor-periscope' ),
+					'help_text' => '',
+					'value'     => 'N/A',
+					'selected'  => false,
+				),
+			)
 		);
 	}
 }
