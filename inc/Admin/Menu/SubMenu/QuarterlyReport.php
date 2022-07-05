f92697fa824dc349ceb6ff5523bae13d2823e5f6
shewa12 <shewa12kpi@gmail.com>
2022-07-05 22:19:47 +0600

    new: quarterly report files

---
+++ inc/Admin/Menu/SubMenu/QuarterlyReport.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Quarterly Report Concrete class
+ *
+ * @package Tutor_Periscope\Admin\SubMenu
+ *
+ * @author Shewa <shewa12kpi@gmail.com>
+ *
+ * @since v1.0.0
+ */
+
+namespace Tutor_Periscope\Admin\Menu\SubMenu;
+
+/**
+ * EvaluationReport sub menu
+ */
+class QuarterlyReport implements SubMenuInterface {
+
+	/**
+	 * Page title
+	 *
+	 * @since v2.0.0
+	 *
+	 * @return string  page title
+	 */
+	public function page_title(): string {
+		return __( 'Quarterly Report', 'tutor-periscope' );
+	}
+
+	/**
+	 * Menu title
+	 *
+	 * @since v2.0.0
+	 *
+	 * @return string  menu title
+	 */
+	public function menu_title(): string {
+		return __( 'Quarterly Report', 'tutor-periscope' );
+	}
+
+	/**
+	 * Capability to access this menu
+	 *
+	 * @since v2.0.0
+	 *
+	 * @return string  capability
+	 */
+	public function capability(): string {
+		return 'manage_options';
+	}
+
+	/**
+	 * Page URL slug
+	 *
+	 * @since v2.0.0
+	 *
+	 * @return string  slug
+	 */
+	public function slug(): string {
+		return 'tutor-periscope-quarterly-report';
+	}
+
+	/**
+	 * Render content for this sub-menu page
+	 *
+	 * @since v2.0.0
+	 *
+	 * @return void
+	 */
+	public function view() {
+		$view = TUTOR_PERISCOPE_VIEWS . 'admin/quarterly-report.php';
+		if ( file_exists( $view ) ) {
+			tutor_load_template_from_custom_path(
+				$view,
+				array()
+			);
+		} else {
+			echo esc_html( "$view file not found", 'tutor-periscope' );
+		}
+	}
+}
---
+++ views/admin/quarterly-report.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Evaluation quarterly report page
+ *
+ * @since v2.0.0
+ *
+ * @package TutorPeriscope\Admin\Page
+ */
+
+?>
+<div class="wrap evaluation-report-wrapper">
+	<h1>
+		<?php esc_html_e( 'Evaluation Report', 'tutor-periscope' ); ?>
+	</h1>
+	<p>
+		<?php esc_html_e( 'Coming soon...', 'tutor-periscope' ); ?>
+	</p>
+</div>
